d13351fe7fa6d44ce6519325d4bd092dfb82d3b6
Radek Raichl <radekraichl@gmail.com>
2025-10-04 00:29:03 +0200

    Cleanup RoomManager.cs

--- DungeonGenerator.Console/RoomManager.cs
+++ DungeonGenerator.Console/RoomManager.cs
@@ -3,17 +3,12 @@ using System.Runtime.CompilerServices;
 
 namespace Dungeon;
 
-internal class RoomManager
+internal class RoomManager(Grid grid)
 {
-    private readonly Grid _grid;
+    private readonly Grid _grid = grid;
     private readonly List<Room> _rooms = [];
 
     public List<Room> Rooms => _rooms;
-
-    public RoomManager(Grid grid)
-    {
-        _grid = grid;
-    }
 
     public void CraveRooms(int min, int max, int numberOfAttempts)
     {
@@ -53,41 +48,6 @@ internal class RoomManager
         }
     }
 
-    //private Point? FindNearestConnector2(Point fromConnector)
-    //{
-    //    var queue = new Queue<(Point pos, int dist)>();
-    //    var visited = new HashSet<Point>();
-    //    queue.Enqueue((fromConnector, 0));
-    //    visited.Add(fromConnector);
-
-    //    while (queue.Count > 0)
-    //    {
-    //        var (pos, dist) = queue.Dequeue();
-
-    //        var tile = _grid.GetTile(pos);
-    //        if (tile.Type == Tile.TileType.RoomConnector)
-    //        {
-    //            // zjistíme, zda connector patří do jiné místnosti než startovní
-    //            var room = _rooms.FirstOrDefault(r => r.Connectors.Contains(pos));
-    //            var startRoom = _rooms.FirstOrDefault(r => r.Connectors.Contains(fromConnector));
-
-    //            if (room != null && room != startRoom && !room.Merged)
-    //                return pos;
-    //        }
-
-    //        foreach (var neighbor in _grid.GetNeighbors4(pos))
-    //        {
-    //            if (!visited.Contains(neighbor) && IsWalkableTile(_grid.GetTile(neighbor).Type))
-    //            {
-    //                visited.Add(neighbor);
-    //                queue.Enqueue((neighbor, dist + 1));
-    //            }
-    //        }
-    //    }
-
-    //    return null; // nic dostupného
-    //}
-
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
     private static bool IsWalkableTile(Tile.TileType type)
     {
@@ -95,7 +55,7 @@ internal class RoomManager
                type == Tile.TileType.Corridor;
     }
 
-    private Point? FindNearestConnector2(Point fromConnector, List<Point> path = null)
+    private Point? FindNearestConnector(Point fromConnector, List<Point> path = null)
     {
         var queue = new Queue<Point>();
         var visited = new HashSet<Point>();
@@ -155,7 +115,6 @@ internal class RoomManager
         return null;
     }
 
-
     public void ConnectRooms()
     {
         // Vybereme náhodně místnost
@@ -178,7 +137,7 @@ internal class RoomManager
                 // Odebereme ho, aby se už nezopakoval
                 uncheckedConnectors.Remove(currentConnector);
                 // Najdeme nejbližší konektor nějaké nemergnuté místnosti
-                nearestConnectorTest = FindNearestConnector2(currentConnector, path);
+                nearestConnectorTest = FindNearestConnector(currentConnector, path);
             }
 
             // Když cesta neexistuje
@@ -195,26 +154,8 @@ internal class RoomManager
             Room targetRoom = _rooms.First(r => r.Connectors.Contains(nearestConnector));
             targetRoom.Merged = true;
 
-            // Smaž počáteční a koncový konektor
-            //targetRoom.Connectors.Remove(currentConnector);
-            //targetRoom.Connectors.Remove(nearestConnector);
-
             // TODO
             //targetRoom.RemoveConnectors(50);
-
-            // Označ začátek i konec chodby
-            //_grid.GetTile(currentConnector).Type = Tile.TileType.CorridorPath;
-            //_grid.GetTile(nearestConnector).Type = Tile.TileType.CorridorPath;
-
-            // Vygeneruj cestu pomocí pathfindingu
-            //var path = new List<Point>();
-            //FindNearestConnector2(currentConnector, path);
-            //_pathFinder.FindPath(currentConnector, nearestConnector, path);
-
-
-            Console.WriteLine($"Room   : {currentRoom.ID}");
-            Console.WriteLine($"Current: {currentConnector}");
-            Console.WriteLine($"Nearest: {nearestConnector}");
 
             foreach (var point in path)
             {
